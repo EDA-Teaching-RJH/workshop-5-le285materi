c092191a77524e904df24eca246eabf79805bac4
le285materi <le285@kent.ac.uk>
2024-10-24 13:19:29 +0000

    commit 1

--- StarTrek.py
+++ StarTrek.py
@@ -31,31 +31,33 @@ def main():
 
 	while True: 
 		display_status() 
-		action = get_user_action() 
 
-	if action == "1": 
-		score += run_mission() 
-	elif action == "2": 
-		repair_system() 
-	elif action == "3": 
-		add_crew_member() 
-	elif action == "4": 
-		print(f"Simulation ended. Final score: {score}") break 
-	else: 
-		print("Invalid action. Please try again.") 
+		action = get_user_action()
+
+		if action == "1": 
+			score += run_mission() 
+		elif action == "2": 
+			repair_system() 
+		elif action == "3": 
+			add_crew_member() 
+		elif action == "4": 
+				print(f"Simulation ended. Final score: {score}")
+		else: 
+			print("Invalid action. Please try again.") 
 		
-	turns += 1 
-	handle_random_event() 
+		turns += 1 
+		handle_random_event() 
 
-	if turns % 3 == 0: 
-		replenish_resources() 
+		if turns % 3 == 0: 
+			replenish_resources() 
 
 def display_status(): 
-# TODO: Implement function to display ship status, resources, and crew 
-
+	# TODO: Implement function to display ship status, resources, and crew 
+      print("Display ship staus, resources, and crew")
 def get_user_action(): 
-# TODO: Implement function to get and return user's chosen action 
-
+	choice = input("1 run mission \n 2 repair system \n 3 add crew member \n 4 final score")
+	return choice 
+	
 def run_mission(): 
 	mission_type = random.choice(MISSION_TYPES) 
 	print(f"\nNew mission: {mission_type}") 
@@ -63,19 +65,18 @@ def run_mission():
 	# Return the score earned from the mission 
 
 def repair_system(): 
-
-# TODO: Implement system repair functionality
- 
+	# TODO: Implement system repair functionality
+    print("system and repair code")
 def add_crew_member(): 
-# TODO: Implement functionality to add a new crew member 
-
+	# TODO: Implement functionality to add a new crew member 
+    print("add a new crew code")
 def handle_random_event():
-# TODO: Implement random events that can occur during the simulation 
-
+	# TODO: Implement random events that can occur during the simulation 
+       print("random events that can occur during the simulation code")
 def use_resource(resource, amount): 
-# TODO: Implement resource usage logic 
-
+	# TODO: Implement resource usage logic 
+    print("resources usage and logic code")
 def replenish_resources(): 
-# TODO: Implement resource replenishment logic 
-
+	# TODO: Implement resource replenishment logic 
+      print("resources replenishemnt logic code")
 main()
